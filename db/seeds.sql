ee664265957c21d8ee5d94ff45fbb176626d229a
Michael Cheroff <michaelcheroff@Michaels-MacBook-Pro.local>
2016-08-06 17:51:27 -0500

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name) VALUES ("Poblano Picasso Burger");
+INSERT INTO burgers(burger_name) VALUES ("Salvador Cauliflower Burger");
+INSERT INTO burgers(burger_name) VALUES ("Shake Your Honeymaker Burger");
+INSERT INTO burgers(burger_name) VALUES ("If Looks Could Kale Burger");
